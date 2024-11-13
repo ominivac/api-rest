2758f62999f9aea4590aa4175d8e07340a9f2367
ominivac <robertokbs@gmail.com>
2024-11-13 00:46:43 -0300

    rota selecoes e mock

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import app from './src/app.js';
 
-const port = 3000
+const PORT = 3000
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
+
 
--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ import express from 'express'
 
 const app = express()
 
+//mock
+const selecoes = [
+  { id: 1, nome: 'Seleção A' },
+  { id: 2, nome: 'Seleção B' },
+  { id: 3, nome: 'Seleção C' },
+  { id: 4, nome: 'Seleção D' },
+]
+
 
 //criar rota padrão
 app.get('/', (req, res) => {
   res.send('curso de nodejs')
+})
+
+app.get('/selecoes', (req, res) => {
+  res.status(200).send(selecoes)
 })
 
 
