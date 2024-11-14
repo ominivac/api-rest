ed3f9ad8231490ed8f343b699f9bc2b0922f788a
ominivac <robertokbs@gmail.com>
2024-11-13 22:13:35 -0300

    metodo post

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from 'express'
 
 const app = express()
 
+//indicar para o expresse ler body com json
+app.use(express.json())
+
 //mock
 const selecoes = [
   { id: 1, nome: 'Seleção A' },
@@ -21,4 +24,9 @@ app.get('/selecoes', (req, res) => {
 })
 
 
+
+app.post('/selecoes', (req, res) => {
+ selecoes.push(req.body)
+ res.status(201).send ('seleção criada com sucesso')
+})
 export default app
