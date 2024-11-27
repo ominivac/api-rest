16ac2adac2ee9e6bb44faea5cdf640323bdbb1c2
ominivac <robertokbs@gmail.com>
2024-11-26 22:04:11 -0300

    Metodo delete

--- src/app.js
+++ src/app.js
@@ -17,25 +17,36 @@ function buscarSelecaoPorId(id) {
   return selecoes.filter(selecao => selecao.id == id)
 }
 
+//pegar a posicao ou index do elemento no array por id
+function buscarIndexSelecao(id) {
+  return selecoes.findIndex(selecao => selecao.id == id)
+}
+
+
 //criar rota padrão
 app.get('/', (req, res) => {
   res.send('curso de nodejs')
 })
 
 app.get('/selecoes/:id', (req, res) => {
-  let index = req.params.id
   res.json(buscarSelecaoPorId(req.params.id))
 })
-
 
 app.get('/selecoes', (req, res) => {
   res.status(200).send(selecoes)
 })
 
-
-
 app.post('/selecoes', (req, res) => {
  selecoes.push(req.body)
  res.status(201).send ('seleção criada com sucesso')
 })
+
+app.delete('/selecoes/:id', (req, res) => {
+  let index = buscarIndexSelecao(req.params.id)
+  selecoes.splice(index, 1)
+  res.status(200).send('seleção deletada com sucesso')
+})
+
+
+
 export default app
