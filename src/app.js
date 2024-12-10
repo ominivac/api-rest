6bb5d1c03aaa456b08e6309786a21966be808bba
ominivac <robertokbs@gmail.com>
2024-12-09 23:27:52 -0300

    metodo insert

--- src/app.js
+++ src/app.js
@@ -35,11 +35,12 @@ app.get('/selecoes/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const sql = 'SELECT * FROM selecoes WHERE id=?;'
   conexao.query(sql, id,( error, result,) => {
+    const linha = result[0]
     if (error) {
       console.log(error)
       res.status(404).json({'Erro': 'dados não encontrados'})
     }else {
-      res.status(200).json(result)
+      res.status(200).json(linha)
     }
    
   })
@@ -47,8 +48,21 @@ app.get('/selecoes/:id', (req, res) => {
 
 
 app.post('/selecoes', (req, res) => {
- selecoes.push(req.body)
- res.status(201).send ('seleção criada com sucesso')
+ //selecoes.push(req.body)
+ //res.status(201).send ('seleção criada com sucesso')
+  const selecao = req.body
+
+  const sql = "INSERT INTO selecoes SET ?;"
+  conexao.query(sql, selecao,( error, result,) => {
+    
+    if (error) {
+      console.log(error)
+      res.status(404).json({'Erro': 'dados não encontrados'})
+    }else {
+      res.status(201).json(result)
+    }
+   
+  })
 })
 
 app.delete('/selecoes/:id', (req, res) => {
