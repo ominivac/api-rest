17ba49c43aac30c03969c3b44128431bdba8f062
ominivac <robertokbs@gmail.com>
2024-12-09 23:48:10 -0300

    limpar comentarios

--- src/app.js
+++ src/app.js
@@ -6,19 +6,7 @@ const app = express()
 //indicar para o expresse ler body com json
 app.use(express.json())
 
-
-
-function buscarSelecaoPorId(id) {
-  return selecoes.filter(selecao => selecao.id == id)
-}
-
-//pegar a posicao ou index do elemento no array por id
-function buscarIndexSelecao(id) {
-  return selecoes.findIndex(selecao => selecao.id == id)
-}
-
 app.get('/selecoes', (req, res) => {
- // res.status(200).send(selecoes)
   const sql = 'SELECT * FROM selecoes;'
   conexao.query(sql, ( error, result,) => {
     if (error) {
@@ -27,7 +15,6 @@ app.get('/selecoes', (req, res) => {
     }else {
       res.status(200).json(result)
     }
-   
   })
 })
 
@@ -42,33 +29,24 @@ app.get('/selecoes/:id', (req, res) => {
     }else {
       res.status(200).json(linha)
     }
-   
   })
 })
 
 
 app.post('/selecoes', (req, res) => {
- //selecoes.push(req.body)
- //res.status(201).send ('seleção criada com sucesso')
   const selecao = req.body
-
   const sql = "INSERT INTO selecoes SET ?;"
   conexao.query(sql, selecao,( error, result,) => {
-    
     if (error) {
       console.log(error)
       res.status(404).json({'Erro': 'dados não encontrados'})
     }else {
       res.status(201).json(result)
     }
-   
   })
 })
 
 app.delete('/selecoes/:id', (req, res) => {
-  // let index = buscarIndexSelecao(req.params.id)
-  // selecoes.splice(index, 1)
-  // res.status(200).send('seleção deletada com sucesso')
   const id = parseInt(req.params.id)
   const sql = 'DELETE FROM selecoes WHERE id=?;'
   conexao.query(sql, id,( error, result,) => {
@@ -78,18 +56,10 @@ app.delete('/selecoes/:id', (req, res) => {
     }else {
       res.status(200).send('seleção deletada com sucesso')
     }
-   
   })
-
 })
 
-
-
 app.put('/selecoes/:id', (req, res) => {
-  // let index = buscarIndexSelecao(req.params.id)
-  // selecoes[index].selecao = req.body.selecao
-  // selecoes[index].nome = req.body.nome
-  // res.json(selecoes)
   const id = parseInt(req.params.id)
   const sql = 'UPDATE selecoes SET ? FROM selecoes WHERE id=?;'
   conexao.query(sql, [selecao, id],( error, result,) => {
@@ -99,9 +69,7 @@ app.put('/selecoes/:id', (req, res) => {
     }else {
       res.status(200).send('seleção deletada com sucesso')
     }
-   
   })
 })
-
 
 export default app
