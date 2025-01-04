989ae48fde5efc8e7c340e253362b0bcd59007c9
ominivac <robertokbs@gmail.com>
2025-01-04 00:38:23 -0300

    Refatoração para MVC

--- src/app.js
+++ src/app.js
@@ -1,75 +1,22 @@
 import express from 'express'
-import conexao  from '../infra/conexao.js'
-
+import conexao  from './app/database/conexao.js'
+import SelecaoController from './app/controllers/SelecaoController.js'
 const app = express()
 
 //indicar para o expresse ler body com json
 app.use(express.json())
 
-app.get('/selecoes', (req, res) => {
-  const sql = 'SELECT * FROM selecoes;'
-  conexao.query(sql, ( error, result,) => {
-    if (error) {
-      console.log(error)
-      res.status(404).json({'Erro': 'dadosnão encontrados'})
-    }else {
-      res.status(200).json(result)
-    }
-  })
-})
+app.get('/selecoes', SelecaoController.index)
 
-app.get('/selecoes/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  const sql = 'SELECT * FROM selecoes WHERE id=?;'
-  conexao.query(sql, id,( error, result,) => {
-    const linha = result[0]
-    if (error) {
-      console.log(error)
-      res.status(404).json({'Erro': 'dados não encontrados'})
-    }else {
-      res.status(200).json(linha)
-    }
-  })
-})
+app.get('/selecoes/:id', SelecaoController.show)
+
+app.post('/selecoes', SelecaoController.store)
+
+app.put('/selecoes/:id',SelecaoController.update) 
+
+app.delete('/selecoes/:id',SelecaoController.delete )
 
 
-app.post('/selecoes', (req, res) => {
-  const selecao = req.body
-  const sql = "INSERT INTO selecoes SET ?;"
-  conexao.query(sql, selecao,( error, result,) => {
-    if (error) {
-      console.log(error)
-      res.status(404).json({'Erro': 'dados não encontrados'})
-    }else {
-      res.status(201).json(result)
-    }
-  })
-})
 
-app.delete('/selecoes/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  const sql = 'DELETE FROM selecoes WHERE id=?;'
-  conexao.query(sql, id,( error, result,) => {
-    if (error) {
-      console.log(error)
-      res.status(404).json({'Erro': error})
-    }else {
-      res.status(200).send('seleção deletada com sucesso')
-    }
-  })
-})
-
-app.put('/selecoes/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  const sql = 'UPDATE selecoes SET ? FROM selecoes WHERE id=?;'
-  conexao.query(sql, [selecao, id],( error, result,) => {
-    if (error) {
-      console.log(error)
-      res.status(404).json({'Erro': error})
-    }else {
-      res.status(200).send('seleção deletada com sucesso')
-    }
-  })
-})
 
 export default app
---
+++ src/app/controllers/SelecaoController.js
@@ -0,0 +1,77 @@
+class SelecaoController {
+
+    index(req, res) {
+        const sql = 'SELECT * FROM selecoes;'
+        conexao.query(sql, ( error, result,) => {
+          if (error) {
+            console.log(error)
+            res.status(404).json({'Erro': 'dadosnão encontrados'})
+          }else {
+            res.status(200).json(result)
+          }
+        })
+    }
+
+    show(req, res) {
+        const id = parseInt(req.params.id)
+        const sql = 'SELECT * FROM selecoes WHERE id=?;'
+        conexao.query(sql, id,( error, result,) => {
+          const linha = result[0]
+          if (error) {
+            console.log(error)
+            res.status(404).json({'Erro': 'dados não encontrados'})
+          }else {
+            res.status(200).json(linha)
+          }
+        })
+    }
+
+    store(req, res) {
+        const selecao = req.body
+        const sql = "INSERT INTO selecoes SET ?;"
+        conexao.query(sql, selecao,( error, result,) => {
+          if (error) {
+            console.log(error)
+            res.status(404).json({'Erro': 'dados não encontrados'})
+          }else {
+            res.status(201).json(result)
+          }
+        })
+
+        
+    update(req, res) {}
+
+    delete(req, res) {}
+
+
+
+    }
+
+    update(req, res) {
+      const id = req.params.id
+      const selecao = req.body
+      const sql = 'UPDATE selecoes SET ? FROM selecoes WHERE id=?;'
+      conexao.query(sql, [selecao, id],( error, result) => {
+        if (error) {
+          res.status(404).json({'Erro': error})
+        }else {
+          res.status(200).send('seleção deletada com sucesso')
+        }
+      })
+    }
+
+    delete(req, res) {
+      const id = parseInt(req.params.id)
+      const sql = 'DELETE FROM selecoes WHERE id=?;'
+      conexao.query(sql, id,( error, result) => {
+        if (error) {
+          res.status(404).json({'Erro': error})
+        }else {
+          res.status(200).send('seleção deletada com sucesso')
+        }
+      })
+    }
+
+
+//padrão singleton
+export default new SelecaoController()
